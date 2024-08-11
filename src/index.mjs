e12589e8f7f98964aaa996611a76b97480732011
unknown <hooss@o365.dge.go.kr>
2024-08-11 19:53:31 +0900

    Added a message detecting system which ignores bot's, not startswith setted prefix or hasn't admin permission

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,25 @@
 import dotenv from 'dotenv';
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, GatewayIntentBits, Events, PermissionsBitField } from 'discord.js';
 
 dotenv.config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds, 
+    GatewayIntentBits.MessageContent, 
+    GatewayIntentBits.GuildMessages 
+]});
 
-client.on('ready', () => {
+client.on(Events.ClientReady, () => {
     console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on(Events.MessageCreate, (msg) => {
+    if (msg.author.bot) return;
+    if (!msg.content.startsWith('!')) return;
+    if (!msg.member.permissions.has(PermissionsBitField.Flags.Administrator)) return;
+
+    const args = msg.content.slice(1).split(' ');
+    const commandName = args[0];
 });
 
 client.login(process.env.TOKEN);
