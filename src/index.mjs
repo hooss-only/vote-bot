af192da6b10c133a2a4b1b47a76f9930d696d884
unknown <hooss@o365.dge.go.kr>
2024-08-11 22:26:58 +0900

    Added vote

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,25 @@
 import dotenv from 'dotenv';
-import { Client, GatewayIntentBits, Events, PermissionsBitField } from 'discord.js';
+import { Client, GatewayIntentBits, Events, PermissionsBitField, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, ComponentType } from 'discord.js';
 
 dotenv.config();
+
+let voteName = "";
+let voteDescription = "";
+let voteChannelID = "";
+
+const up = new ButtonBuilder()
+    .setCustomId("up")
+    .setLabel('찬')
+    .setStyle(ButtonStyle.Success);
+
+const down = new ButtonBuilder()
+    .setCustomId("down")
+    .setLabel('반')
+    .setStyle(ButtonStyle.Danger);
+                    
+const row = new ActionRowBuilder()
+    .addComponents(up, down);
+
 
 const client = new Client({ intents: [
     GatewayIntentBits.Guilds, 
@@ -20,6 +38,60 @@ client.on(Events.MessageCreate, (msg) => {
 
     const args = msg.content.slice(1).split(' ');
     const commandName = args[0];
+
+    if (commandName == "투표") {
+        if (args.length < 2 || !['시작', '제목', '내용', '결산', '채널'].includes(args[1])) {
+            msg.reply("!투표 (시작 / 제목 / 내용 / 채널 / 결산)");
+            return;
+        }
+
+        switch (args[1]) {
+            case "시작":
+                const embed = new EmbedBuilder()
+                    .setTitle(voteName)
+                    .setTimestamp()
+                    .setDescription(voteDescription)
+                    .setFooter({ text: "익명 투표봇 by hooss" });
+
+                client.channels.cache.get(voteChannelID).send({ embeds: [embed], components: [row] });
+                break;
+            
+            case "제목":
+                if (args.length < 3) {
+                    msg.reply("!투표 (제목 / 내용) <TEXT>");
+                    break;
+                }
+                voteName = args.slice(2).join(' ');
+                break;
+
+            case "내용":
+                if (args.length < 3) {
+                    msg.reply("!투표 (제목 / 내용) <TEXT>");
+                    break;
+                }
+                voteDescription = args.slice(2).join(' ');
+                break;
+            
+            case "채널":
+                if (args.length < 3) {
+                    msg.reply("!투표 채널 <ID>");
+                    break;
+                }
+                voteChannelID = args[2];
+                break;
+
+            case "결산":
+                break;
+        }
+    }
+});
+
+client.on("interactionCreate", (interaction) => {
+    if (!interaction.isButton) return;
+    
+    console.log(interaction.customId);
+
+    return;
 });
 
 client.login(process.env.TOKEN);
